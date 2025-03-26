b8557234b1f2a6612f24be9683b9e4c5f82c44a4
Eurekamps <4yony4@gmail.com>
2025-03-26 11:35:19 +0100

    Parte 4 tutorial completo, fisica

--- src/main.js
+++ src/main.js
@@ -5,10 +5,17 @@ import { AUTO, Scale,Game } from 'phaser';
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config = {
     type: AUTO,
-    width: 1024,
-    height: 768,
+    width: 800,
+    height: 600,
     parent: 'game-container',
-    backgroundColor: '#028af8',
+    //backgroundColor: '#028af8',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scale: {
         mode: Scale.FIT,
         autoCenter: Scale.CENTER_BOTH
