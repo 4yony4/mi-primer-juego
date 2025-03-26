64944c39079950de62937fd61a0414ebddb95202
Eurekamps <4yony4@gmail.com>
2025-03-26 10:12:25 +0100

    primer commit

---
+++ src/scenes/Game.js
@@ -0,0 +1,36 @@
+import { Scene } from 'phaser';
+
+export class Game extends Scene
+{
+    constructor ()
+    {
+        super('Game');
+    }
+
+    preload ()
+    {
+        this.load.setPath('assets');
+        
+        this.load.image('background', 'bg.png');
+        this.load.image('logo', 'logo.png');
+
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    }
+
+    create ()
+    {
+        
+        this.add.image(512, 384, 'background');
+        this.add.image(512, 350, 'logo').setDepth(100);
+        this.add.text(512, 490, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
+            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5).setDepth(100);
+        
+    }
+}
